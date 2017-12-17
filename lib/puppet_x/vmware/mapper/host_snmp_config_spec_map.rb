e4e39a9f3a72ab706feada17c738d343af4c400c
Nick Maludy <nmaludy@gmail.com>
2017-12-16 23:34:20 -0500

    Adding mapper for SNMP config spec

---
+++ lib/puppet_x/vmware/mapper/host_snmp_config_spec_map.rb
@@ -0,0 +1,58 @@
+# Copyright (C) 2013 VMware, Inc.
+
+require 'set'
+
+module PuppetX::VMware::Mapper
+
+  class HostSnmpConfigSpecMap < Map
+    def initialize
+      @initTree = {
+        Node => NodeData[
+          :node_type => 'HostSnmpConfigSpec',
+        ],
+        :option => LeafData[
+          :prop_name => PROP_NAME_IS_FULL_PATH,
+	        :desc => 'Array KeyValue options.',
+          :olio => {
+            Puppet::Property::VMware_Array_VIM_Object => {
+              :property_option => {
+                :type => 'KeyValue',
+                :array_matching => :all,
+                :comparison_scope => :array_element,
+                :key => [:key],
+              },
+            },
+          },
+        ],
+	      :readOnlyCommunity => LeafData[
+	        :prop_name => PROP_NAME_IS_FULL_PATH,
+	        :desc => 'Array of read-only Community strings. For example: ["abc123", "xxx555"]',
+	        :olio => {
+	          Puppet::Property::VMware_Array => {
+	            :property_option => {
+	    	        :inclusive => :true,
+		            :preserve  => :false,	
+  	          },
+	          },
+	        },
+	      ],
+        :trapTargets => LeafData[
+          :prop_name => PROP_NAME_IS_FULL_PATH,
+	        :desc => 'Array HostSnmpDestination objects that define the SNMP trap targets',
+          :olio => {
+            Puppet::Property::VMware_Array_VIM_Object => {
+              :property_option => {
+                :type => 'HostSnmpDestination',
+                :array_matching => :all,
+                :comparison_scope => :array_element,
+                :key => [:hostName, :port],
+              },
+            },
+          },
+        ],
+      }
+      super
+    end
+  end
+end
+
